d0abff2ba56b8f765c256c33a4ba17867eb8fb1b
WillianBoita <wkboita@gmail.com>
2026-04-07 14:44:57 -0300

    atualizando validação de tokens

---
+++ auth-api/src/modules/user/middleware/authentication.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from 'express'
+import AuthException from '../middleware/AuthException.js'
+import jwt from 'jsonwebtoken'
+
+export default async function authentication(req: Request, res: Response, next: NextFunction) {
+  try {
+    const auth = req.headers?.authorization?.split(" ");
+
+    const [, token] = auth ?? ["Bearer", '']
+
+    
+    if(!token) {
+      throw new AuthException(401, "Token não informado.")
+    }
+    
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as any;
+    req.authUser = decoded.authUser
+
+    return next();
+  } catch (err: any) {
+    return {
+      status: err.status ? err.status : 500,
+      message: err.message 
+    }
+  }
+}
